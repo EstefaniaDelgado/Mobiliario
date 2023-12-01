44ef5beaad726594c3b9783f7d34dbdbef0296f4
EstefaniaDelgado <teff_64@hotmail.com>
2023-12-01 08:17:15 -0500

    Cambios al Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavbarComp from './components/NavbarComp'
-import BrandExample from './components/Prueba'
 
 
 
@@ -13,7 +12,7 @@ function App() {
   return (
     <>
     <NavbarComp/>
-{/* <BrandExample/> */}
+
     </>
   )
 }
--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -5,13 +5,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LogoVivanti from '../assets/logo-vivanti.svg'
+import { Col, Row } from 'react-bootstrap';
 
 const NavbarComp = () => {
   return (
-    <div>
-         <Navbar expand="md" className="bg-body-tertiary">
+    <header>
+         <Navbar expand="sm" className="bg-body-tertiary">
       <Container fluid>
-      <Container>
+     <Row className='w-100 align-items-center m-0'>
+      <Col>
+      <Container className='d-flex justify-content-between align-items-center'>
           <Navbar.Brand href="#home">
             <img
               alt=""
@@ -22,20 +25,14 @@ const NavbarComp = () => {
             />{' '}
           Mobiliario Vivanti
           </Navbar.Brand>
+          {/* icon burger */}
+          <Navbar.Toggle aria-controls="navbarScroll" />
         </Container>
-      
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Sobre Nosotros</Nav.Link>
-            <Nav.Link href="#action2">Proyectos</Nav.Link>
-            <Nav.Link href="#action2">Servicios</Nav.Link>
-            <Nav.Link href="#action2">Clientes</Nav.Link>
-          {/*   <NavDropdown title="Link" id="navbarScrollingDropdown">
+        </Col>
+        
+        {/* select & searchbar */}
+        <Col className='d-flex gap-2 align-items-center'>
+    <NavDropdown title="Seleccionar" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -44,17 +41,43 @@ const NavbarComp = () => {
               <NavDropdown.Item href="#action5">
                 Something else here
               </NavDropdown.Item>
-            </NavDropdown> */}
-            {/* <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link> */}
+            </NavDropdown>  
+      
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          </Col>
+
+        <Col>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Nosotros</Nav.Link>
+            <Nav.Link href="#action2">Proyectos</Nav.Link>
+            <Nav.Link href="#action2">Servicios</Nav.Link>
+            <Nav.Link href="#action2">Clientes</Nav.Link>
+            
+       
           </Nav>
        
         </Navbar.Collapse>
+        </Col>
+
+
+        </Row>
       </Container>
     </Navbar>
       
-    </div>
+    </header>
   )
 }
 
