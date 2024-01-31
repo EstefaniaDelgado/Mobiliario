2799d8249a173ef3a551d4379c5e2ebe56a6aa74
EstefaniaDelgado <teff_64@hotmail.com>
2024-01-30 20:00:54 -0500

    changes products

--- src/components/Products/components/index.jsx
+++ src/components/Products/components/index.jsx
@@ -6,16 +6,20 @@ const Product = () => {
   return (
     <section className={styles.containerProducts}>
         <article className={styles.product}>
-     <img src={LivingRoom} alt="Sala" className={styles.livingRoom} />
+     <img src={LivingRoom} alt="Sala" className={styles.productItem} />
+     <p className={styles.nameProduct}>Decoración</p>
      </article>
      <article className={styles.product}>
-     <img src={DiningRoom} alt="Sala" className={styles.livingRoom} />
+     <img src={DiningRoom} alt="Sala" className={styles.productItem} />
+     <p className={styles.nameProduct}>Lenceria Dormitorio</p>
      </article>
      <article className={styles.product}>
-     <img src={DiningRoom} alt="Sala" className={styles.livingRoom} />
+     <img src={DiningRoom} alt="Sala" className={styles.productItem} />
+     <p className={styles.nameProduct}>Iluminación</p>
      </article>
      <article className={styles.product}>
-     <img src={DiningRoom} alt="Sala" className={styles.livingRoom} />
+     <img src={DiningRoom} alt="Sala" className={styles.productItem} />
+     <p className={styles.nameProduct}>Cortineria</p>
      </article>
 
     </section>
