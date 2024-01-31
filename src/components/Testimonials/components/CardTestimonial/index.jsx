f39d681ed0159df7bb66ca232a813efbfbea9734
EstefaniaDelgado <teff_64@hotmail.com>
2024-01-31 11:44:30 -0500

    testimonials seccion

--- src/components/Testimonials/components/CardTestimonial/index.jsx
+++ src/components/Testimonials/components/CardTestimonial/index.jsx
@@ -1,46 +1,95 @@
-import styles from './Testimonial.module.css'
+import styles from './Testimonial.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCoverflow,
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-coverflow';
+import { useEffect, useState } from 'react';
 
-const Testimonial = () => {
-  return (
-    <div class="carousel">
-    <div class="swiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide swiper-slide--one">
-          <div>
-            <h2>1 Series</h2>
-            <a>explore</a>
-          </div>
-        </div>
-        <div class="swiper-slide swiper-slide--two">
-          <div>
-            <h2>2 Series</h2>
-            <a>explore</a>
-          </div>
-        </div>
-        <div class="swiper-slide swiper-slide--three">
-          <div>
-            <h2>3 Series</h2>
-            <a>explore</a>
-          </div>
-        </div>
-        <div class="swiper-slide swiper-slide--four">
-          <div>
-            <h2>4 Series</h2>
-            <a>explore</a>
-          </div>
-        </div>
+const Testimonial = ({ slides }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-        <div class="swiper-slide swiper-slide--five">
-          <div>
-            <h2>5 Series</h2>
-            <a>explore</a>
-          </div>
-        </div>
-      </div>
-      <div class="swiper-pagination"></div>
-    </div>
-  </div>
-  )
-}
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
-export default Testimonial
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMobile]);
+
+  const tabletScreem = (
+    <Swiper
+      effect={'coverflow'}
+      grabCursor={true}
+      centeredSlides={true}
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
+      coverflowEffect={{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <div className={styles.containerCarousel}>
+            <img
+              src={`https://unavatar.io/${slide.customer}`}
+              alt="Testimonio"
+              className={styles.imageCarousel}
+            />
+            <p className={styles.review}>{slide.review}</p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+
+  const mobileScreem = (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <div className={styles.containerCarousel}>
+            <img
+              src={`https://unavatar.io/youtube/${slide.customer}`}
+              alt="Testimonio"
+              className={styles.imageCarousel}
+            />
+            <p className={styles.review}>{slide.review}</p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+
+  return <>{isMobile ? mobileScreem : tabletScreem}</>;
+};
+
+export default Testimonial;
--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,14 +1,19 @@
 import styles from './Testimonials.module.css'
 import Testimonial from './components/CardTestimonial';
+import seriesData from '../../data/testimonials'
+console.log(seriesData)
+
 
 
 const Testimonials = () => {
+
+  
   return (
-    <div>
-      <h2 className={styles.subtitle}>Testimonios</h2>
-      <Testimonial/>
-    </div>
-  )
+   <section>
+    <h2 className={styles.subtitle}>Testimonios</h2>
+    <Testimonial slides={seriesData}/>
+   </section>
+  );
 }
 
 export default Testimonials;
